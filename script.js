d991ddb472e73f6be4cb3d3c37c2e1b3a121c5f7
Josh <160084551+clouded-afk@users.noreply.github.com>
2024-07-03 17:29:48 -0400

    Add game controller function so game is playable in the console, win conditions have not been set

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function Gameboard() {
     const columns = 3;
     const board = [];
 
-    const generateBoard = function() {
+    const generateBoard = () => {
         for (let i = 0; i < rows; i++) {
             board[i] = [];
             for (let j = 0; j < columns; j++) {
@@ -14,19 +14,89 @@ function Gameboard() {
 
     const getBoard = () => board;
 
+    const placeMarker = (row, column, player) => {
+        const cell = board[row][column];
 
-    return { generateBoard, getBoard }
-}
+        if (cell.getValue() === "") {
+            cell.addMarker(player)
+            return true; 
+        }
+        return false;
+    }
 
+    const printBoard = () => {
+        const boardWithCellValues =  board.map((row) => row.map((cell) => cell.getValue()));
+        console.log(boardWithCellValues);
+    }
+
+    generateBoard();
+
+    return {
+        getBoard,
+        placeMarker,
+        printBoard
+    };
+};
 
 function Cell() {
     let value = "";
 
-    const placeMarker = (player) => {
+    const addMarker = (player) => {
         value = player;
-    };
+    }
 
     const getValue = () => value;
 
-    return { placeMarker, getValue };
+    return {
+        addMarker,
+        getValue
+    };
 };
+
+function GameController(playerOneName = "Player One", playerTwoName = "Player Two") {
+    const board = Gameboard();
+
+    const players = [
+        {
+            name: playerOneName,
+            marker: "X"
+        },
+        {
+            name: playerTwoName,
+            marker: "O"
+        }
+    ]
+
+    let currentPlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
+    };
+
+    const getCurrentPlayer = () => currentPlayer;
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getCurrentPlayer().name}'s turn.`);
+    };
+
+    const playRound = (row, column) => {
+        console.log(`Placed ${getCurrentPlayer().name}'s marker in row ${row}, column ${column}`);
+        board.placeMarker(row, column, getCurrentPlayer().marker);
+
+        // Win conditions
+
+        switchPlayerTurn();
+        printNewRound();
+    } 
+
+    printNewRound();
+
+    return {
+        playRound,
+        getCurrentPlayer
+    };
+};
+
+
+const game = GameController();
